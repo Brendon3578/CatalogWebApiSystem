a749859dcbcafb88408abd0bc8c4e4ccc59cfe09
Brendon Gomes <brendongomes40@gmail.com>
2024-12-27 23:15:19 -0300

    Create category and product model

---
+++ CatalogWebApiSystem/Domain/Models/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace CatalogWebApiSystem.Domain.Models;
+
+// Classes anêmicas (só com propriedades)
+public class Category
+{
+    public int CategoryId { get; set; }
+    public string? Name { get; set; }
+    public string? ImageUrl { get; set; }
+}
