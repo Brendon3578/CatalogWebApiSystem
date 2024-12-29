1138a93656008705d0764492f26cf972040b0c56
Brendon Gomes <brendongomes40@gmail.com>
2024-12-29 20:46:20 -0300

    Update entities, use data annotations to enhance table data types perfomance and validation

--- CatalogWebApiSystem/Domain/Models/Category.cs
+++ CatalogWebApiSystem/Domain/Models/Category.cs
@@ -1,8 +1,11 @@
 ﻿using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace CatalogWebApiSystem.Domain.Models;
 
 // Classes anêmicas (só com propriedades - sem comportamento)
+[Table("Categories")]
 public class Category
 {
     public Category()
@@ -10,8 +13,16 @@ public class Category
         Products = new Collection<Product>();
     }
 
+    [Key]
     public int CategoryId { get; set; }
+
+    [Required]
+    [StringLength(80)]
     public string? Name { get; set; }
+
+    [Required]
+    [Url]
+    [StringLength(256)]
     public string? ImageUrl { get; set; }
 
     public ICollection<Product>? Products { get; set; }
--- CatalogWebApiSystem/Domain/Models/Product.cs
+++ CatalogWebApiSystem/Domain/Models/Product.cs
@@ -1,13 +1,33 @@
-﻿namespace CatalogWebApiSystem.Domain.Models;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
+namespace CatalogWebApiSystem.Domain.Models;
+
+[Table("Products")]
 public class Product
 {
+    [Key]
     public int ProductId { get; set; }
+
+    [Required]
+    [StringLength(80)]
     public string? Name { get; set; }
+
+    [Required]
+    [StringLength(256)]
     public string? Description { get; set; }
+
+    [Required]
+    [Url]
+    [StringLength(256)]
     public string? ImageUrl { get; set; }
+
+    [Required]
+    [Column(TypeName = "decimal(10,2)")]
     public decimal Price { get; set; }
+
     public float Stock { get; set; }
+
     public DateTime CreatedOn { get; set; }
 
     public int CategoryId { get; set; }
